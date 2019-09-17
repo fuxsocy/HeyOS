0a82a8dcbd4819d8f67212a6114096b7ab25f2f9
Marco Wang <m.aesophor@gmail.com>
2019-09-17 11:53:05 +0800

    kernel: main: load gdt and idt

--- kernel/main.cc
+++ kernel/main.cc
@@ -1,3 +1,9 @@
+// Copyright (c) 2019 FunSociety
+#include <gdt.h>
+#include <idt.h>
+
 extern "C" void start_kernel(const void* multiboot_structure) {
+  heyos::Gdt gdt;
+  heyos::Idt idt(&gdt);
   while (1);
 }
